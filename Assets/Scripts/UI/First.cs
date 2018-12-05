6b94fffae21aa01b9251fbfbf1d53845bccabdda
wojiaolihailong <443249128@qq.com>
2018-12-05 17:50:29 +0800

    新添加的

---
+++ Assets/Scripts/UI/First.cs
@@ -0,0 +1,22 @@
+﻿
+/***
+ * 测试脚本，创建出UI  
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using GameFramework.Fsm;
+using GameFramework.Procedure;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class First : ProcedureBase 
+{
+    protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
+    {
+        base.OnEnter(procedureOwner);
+        UIComponent UI = UnityGameFramework.Runtime.GameEntry.GetComponent<UIComponent>();
+        UI.OpenUIForm("Assets/Prefab/Image.prefab", "UIFirst"); 
+    }
+}
---
+++ Assets/Scripts/UI/UIFormDemo.cs
@@ -0,0 +1,18 @@
+﻿
+/***
+ *  UI挂脚本基类
+ *   
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class UIFormDemo : UIFormLogic 
+{
+
+	
+	 
+}
