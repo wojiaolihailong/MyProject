085c020e9420f7d4cd5633850a26b6f96541cdad
wojiaolihailong <807444242@qq.com>
2018-12-05 23:14:38 +0800

    可以包含多个canvas

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGameFramework.Runtime;
+
+public class UIController : MonoBehaviour {
+
+	 
+	void Start ()
+    {
+		this.GetComponent<Button>().onClick.AddListener(() =>
+		{
+		    UIComponent UI = UnityGameFramework.Runtime.GameEntry.GetComponent<UIComponent>();
+
+		    UI.OpenUIForm("Assets/Prefab/UI_Menu 1.prefab", "UIFirst");
+		     
+		});
+	}
+	
+	 
+}
