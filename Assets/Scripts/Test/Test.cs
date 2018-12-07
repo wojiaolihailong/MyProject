b1070e1f70611d2690d273fdfb26afc63737263d
wojiaolihailong <443249128@qq.com>
2018-12-07 10:57:03 +0800

    添加测试脚本

---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,37 @@
+﻿
+/***
+ * 
+ 
+ *   
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using GameFramework;
+using UnityEngine;
+
+
+public class Show
+{
+    public string Name;
+    public int Age { set; get; } 
+    public Show(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+
+
+public class Test : MonoBehaviour 
+{
+
+	
+	void Start ()
+	{
+		Show s1=new Show("lhl",1);
+       
+	} 
+}
